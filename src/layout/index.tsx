7ffe8b9424d5df821a2b864838c95d071e095921
baharma <bagusaditya1899@gmail.com>
2024-10-05 01:22:31 +0800

    feat(ui):setup

---
+++ src/layout/index.tsx
@@ -0,0 +1,16 @@
+import { ReactNode } from "react"
+
+type Props = {
+    children: ReactNode
+    customClass?: string
+}
+
+const Index = ({children,customClass}:Props) => {
+
+
+    return (
+        <>
+            
+        </>
+    )
+}
